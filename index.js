cfbf7627eb8ebdda66649cb9bdcd87d51968069c
elians7 <114128326+elians7@users.noreply.github.com>
2022-09-21 23:18:09 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Ya que me dijiste lo de Google fotos lo volvi mas original')
+    alert('Para comunicarte de una forma fuera de lo comun lo mucho que me gustas')
+    alert('Andreita ❤️ME ENCANTAAAAAAAAS ❤️')
+})
